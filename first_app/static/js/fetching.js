748692c3b11f79fbd3b7606c9548db6959b744c7
mazurasdf <mazurasdf@gmail.com>
2021-01-26 16:28:38 -0600

    first commit

---
+++ first_app/static/js/fetching.js
@@ -0,0 +1,17 @@
+console.log("hello from the js file!");
+
+function makeCall(id){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res.name);
+            document.getElementById("pkmnName").innerHTML = res.name;
+        })
+        .catch(err => console.log(err));
+}
+
+
+function getPokemon(){
+    let this_id = document.getElementById("idBox").value;
+    makeCall(this_id);
+}
